63c1210bc169be17f1e47beb7b420dfc58625d3a
noradlb1 <zcyfkhuru.bz@mail.ru>
2021-12-27 17:41:35 +0100

    Auto-Downloader-Zippyshare-Part-3-Advanced-SRC-
    Auto-Downloader-Zippyshare-Part-3-Advanced-SRC-

---
+++ Auto Download Zippyshare/Form1.Designer.vb
@@ -0,0 +1,264 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.UkuranFile = New System.Windows.Forms.Label()
+        Me.kecepatandunlud = New System.Windows.Forms.Label()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar3 = New System.Windows.Forms.ProgressBar()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.FileNama = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(602, 123)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(26, 25)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.Size = New System.Drawing.Size(42, 29)
+        Me.WebBrowser1.TabIndex = 0
+        Me.WebBrowser1.Visible = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(78, 16)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(281, 22)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(365, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(86, 29)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "&Download"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar1.Location = New System.Drawing.Point(153, 177)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(270, 16)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(429, 177)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 16)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "0%"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(34, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 16)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "URL:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 89)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 16)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Filename:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(40, 120)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 16)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Size:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(26, 148)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 16)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Speed:"
+        '
+        'UkuranFile
+        '
+        Me.UkuranFile.AutoSize = True
+        Me.UkuranFile.Location = New System.Drawing.Point(87, 120)
+        Me.UkuranFile.Name = "UkuranFile"
+        Me.UkuranFile.Size = New System.Drawing.Size(12, 16)
+        Me.UkuranFile.TabIndex = 10
+        Me.UkuranFile.Text = "."
+        '
+        'kecepatandunlud
+        '
+        Me.kecepatandunlud.AutoSize = True
+        Me.kecepatandunlud.Location = New System.Drawing.Point(170, 148)
+        Me.kecepatandunlud.Name = "kecepatandunlud"
+        Me.kecepatandunlud.Size = New System.Drawing.Size(12, 16)
+        Me.kecepatandunlud.TabIndex = 11
+        Me.kecepatandunlud.Text = "."
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar2.Location = New System.Drawing.Point(1, 197)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(467, 10)
+        Me.ProgressBar2.TabIndex = 13
+        '
+        'ProgressBar3
+        '
+        Me.ProgressBar3.Location = New System.Drawing.Point(90, 148)
+        Me.ProgressBar3.Name = "ProgressBar3"
+        Me.ProgressBar3.Size = New System.Drawing.Size(74, 16)
+        Me.ProgressBar3.TabIndex = 14
+        '
+        'Label6
+        '
+        Me.Label6.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 177)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(135, 16)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Download Progress:"
+        '
+        'FileNama
+        '
+        Me.FileNama.BackColor = System.Drawing.SystemColors.Control
+        Me.FileNama.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.FileNama.Cursor = System.Windows.Forms.Cursors.Default
+        Me.FileNama.Location = New System.Drawing.Point(90, 90)
+        Me.FileNama.Name = "FileNama"
+        Me.FileNama.Size = New System.Drawing.Size(361, 15)
+        Me.FileNama.TabIndex = 16
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(9, 54)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(105, 16)
+        Me.Label7.TabIndex = 19
+        Me.Label7.Text = "Save Directory:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox2.Location = New System.Drawing.Point(120, 51)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(239, 22)
+        Me.TextBox2.TabIndex = 17
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(365, 48)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(86, 29)
+        Me.Button2.TabIndex = 18
+        Me.Button2.Text = "&Browse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(468, 207)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.FileNama)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.ProgressBar3)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.kecepatandunlud)
+        Me.Controls.Add(Me.UkuranFile)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Auto Download ZippyShare Part 3"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents UkuranFile As System.Windows.Forms.Label
+    Friend WithEvents kecepatandunlud As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar2 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgressBar3 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents FileNama As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ Auto Download Zippyshare/Form1.vb
@@ -0,0 +1,164 @@
+﻿Imports System.Net
+
+Public Class Form1
+    Dim LagiDunlud As Boolean = True
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If Button1.Text = "Download" Then
+            CUT = 1
+            LagiDunlud = True
+            WebBrowser1.Navigate(TextBox1.Text)
+            Button1.Text = "Cancel"
+            TextBox1.Enabled = False
+        Else
+            LagiDunlud = False
+            Button1.Text = "Download"
+            TextBox1.Enabled = True
+        End If
+    End Sub
+    Dim WithEvents Dwn As Net.WebClient
+    Dim CUT As Integer = 1, sz As String
+    Private Sub WebBrowser1_DocumentCompleted(ByVal sender As Object, ByVal e As System.Windows.Forms.WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        If CUT = 1 Then
+            CUT += 1
+            Try
+                For Each Ghtm As HtmlElement In WebBrowser1.Document.GetElementsByTagName("a")
+                    If Ghtm.GetAttribute("href").StartsWith("/d/") Then
+                        Dim Spl As String() = Split(TextBox1.Text, "/v/")
+                        Dim LinkDownload As String = Spl(0) & Ghtm.GetAttribute("href")
+                        Dim Info As String() = Split(GetNameAndSize(LinkDownload), "|")
+
+                        FileNama.Text = Info(0)
+                        UkuranFile.Text = Info(1)
+                        sz = Info(1)
+                        WebBrowser1.Navigate("about:blank")
+                        WebBrowser1 = New WebBrowser
+                        WebBrowser1.ScriptErrorsSuppressed = True
+
+                        Try : IO.Directory.CreateDirectory(TextBox2.Text) : Catch : End Try
+                        TextBox2.Text = TextBox2.Text & "\" & Info(0)
+
+                        Dwn = New Net.WebClient ' jangan lupa kasih ini biar gak gagal downloadnya!
+                        Dwn.DownloadFileAsync(New Uri(LinkDownload), TextBox2.Text)
+                    End If
+                Next
+            Catch : End Try
+        End If
+    End Sub
+    Function GetNameAndSize(ByVal Link As String) As String
+        Dim HttpWebReq As HttpWebRequest, HttpWebRes As HttpWebResponse = Nothing, TempStr As String = String.Empty
+        'Try
+        HttpWebReq = WebRequest.Create(Link)
+        HttpWebReq.Timeout = 8000
+        HttpWebReq.Proxy = Nothing
+        HttpWebReq.KeepAlive = False
+        HttpWebReq.AllowAutoRedirect = True
+        HttpWebRes = HttpWebReq.GetResponse
+        'Catch : End Try
+        Dim Sr As IO.StreamReader = New IO.StreamReader(WebBrowser1.DocumentStream)
+        Dim Count As Integer = 0
+        For Each Gtt As String In Split(Sr.ReadToEnd, ";"">")
+            Dim SPL2 As String() = Split(Gtt, "</font>")
+            If SPL2(0) = "Name:" Then
+                Count += 1
+            ElseIf Count = 1 Then
+                Count = 0
+                If SPL2(0).StartsWith("<img") Then
+                    Dim SPLTep As String() = Split(HttpWebRes.Headers.Item(0), "filename")
+                    TempStr = SPLTep(1).Replace("*=UTF-8''", String.Empty).Replace("=", String.Empty) & "|"
+                Else
+                    TempStr = SPL2(0) & "|"
+                End If
+            End If
+        Next
+        Dim Tmpp As String = GetUkuran(HttpWebRes.ContentLength)
+        HttpWebReq.Abort()
+        HttpWebReq = Nothing
+        HttpWebRes.Close()
+        HttpWebRes = Nothing
+        Return TempStr & Tmpp
+    End Function
+    Function GetUkuran(ByVal Sz As Object) As String
+        Dim fsize As Long = Convert.ToInt64(Sz), Fzn As String = String.Empty
+        If fsize > 1073741824 Then
+            Dim size As Double = fsize / 1073741824
+            Fzn = Math.Round(size, 2).ToString & "GB"
+        ElseIf fsize > 1048576 Then
+            Dim size As Double = fsize / 1048576
+            Fzn = Math.Round(size, 2).ToString & "MB"
+        ElseIf fsize > 1024 Then
+            Dim size As Double = fsize / 1024
+            Fzn = Math.Round(size, 2).ToString & "KB"
+        Else
+            Fzn = fsize.ToString & "B"
+        End If
+        Return Fzn
+    End Function
+    Dim Tm As Integer = 0
+    Dim TmpByts As Long = 0
+    Private Sub Dwn_DownloadProgressChanged(ByVal sender As Object, ByVal e As System.Net.DownloadProgressChangedEventArgs) Handles Dwn.DownloadProgressChanged
+        If Not LagiDunlud Then
+            Dwn.CancelAsync()
+            Exit Sub
+        End If
+        ProgressBar1.Value = e.ProgressPercentage
+        ProgressBar3.Maximum = Convert.ToInt32(e.TotalBytesToReceive)
+        Dim Spedcek As Integer
+        If sz.EndsWith("KB") Then
+            Spedcek = 1
+        ElseIf sz.EndsWith("B") Then
+            Spedcek = 1
+        Else
+            Spedcek = 500
+        End If
+        If Tm = 500 Then ' 500ms,1000 = 1sec
+            Tm = 0
+            Dim fsize As Long
+            If Not TmpByts = 0 Then
+                fsize = e.BytesReceived - TmpByts
+            Else
+                fsize = e.BytesReceived
+            End If
+            TmpByts = e.BytesReceived
+            kecepatandunlud.Text = GetUkuran(fsize)
+            Try : ProgressBar3.Value = Convert.ToInt32(fsize) : Catch : End Try
+        Else
+            Tm += 1
+        End If
+        Label1.Text = ProgressBar1.Value & "%"
+        UkuranFile.Text = GetUkuran(e.BytesReceived) & "/" & sz
+        If ProgressBar1.Value = 100 Then Button1.Text = "Download"
+    End Sub
+
+
+    Private Sub WebBrowser1_ProgressChanged(ByVal sender As Object, ByVal e As System.Windows.Forms.WebBrowserProgressChangedEventArgs) Handles WebBrowser1.ProgressChanged
+        ProgressBar2.Maximum = e.MaximumProgress
+        Try
+            ProgressBar2.Value = e.CurrentProgress
+        Catch : End Try
+    End Sub
+
+
+    Private Sub FileNama_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles FileNama.KeyPress
+        e.Handled = True
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim Ofd As New FolderBrowserDialog
+        With Ofd
+            .Description = "Choose Directory"
+            If .ShowDialog = Windows.Forms.DialogResult.OK Then
+                TextBox2.Text = .SelectedPath
+            End If
+        End With
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextBox2.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop).Replace("\Desktop", "\Downloads")
+    End Sub
+
+    Private Sub TextBox2_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox2.KeyPress
+        If Button1.Text = "Cancel" Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
